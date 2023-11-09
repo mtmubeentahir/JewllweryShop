e0c1dfcaf24beb6e2423a6ff7b0e2f5ab8bd5c10
Mubeen <mt.mubeentahir@gmail.com>
2023-11-09 14:10:32 +0500

    initiated Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create!(name: 'Rings')
+Category.create!(name: 'bangles')
+Category.create!(name: 'Bracelates')
+Category.create!(name: 'EarRings')
